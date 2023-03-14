ede543f7c0754c3dfc78ab99952392480064ee40
Conrado Becker <53865252+conrado1008@users.noreply.github.com>
2023-03-14 16:17:21 -0300

    Create linha.h

---
+++ Remote_Control_Car/linha.h
@@ -0,0 +1,19 @@
+/*
+ * linha.h
+ *
+ *  Created on: 22 de nov de 2022
+ *      Author: Conrado
+ */
+
+
+#include <stdint.h>
+
+#ifndef LINHA_H_
+#define LINHA_H_
+
+void config_timer_B0();
+void linha(uint8_t l);
+
+
+
+#endif /* LINHA_H_ */
